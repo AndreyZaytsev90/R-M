2e0025f7c629477fb76ad844ed671fc37a19c9f1
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2026-02-07 16:53:58 +0300

    Добавлены стили и сделан небольшой рефакторинг

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import { Outlet } from 'react-router';
 import './App.css';
-import { Footer } from './shared/Footer/Footer';
-import { Header } from './shared/Header/Header';
+import { Footer } from './shared/components/Footer/Footer';
+import { Header } from './shared/components/Header/Header';
 
 function App() {
   return (
-    <>
-      <Header />
-      <Outlet />
-      <Footer />
-    </>
+    <div className='main-page'>
+      <header className='main-page__header'>
+        <Header />
+      </header>
+      <main className='main-page__content'>
+        <Outlet />
+      </main>
+      <footer className='main-page__footer'>
+        <Footer />
+      </footer>
+    </div>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App.tsx';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router';
 import { CharactersList } from './pages/CharactersList/CharactersList.tsx';
