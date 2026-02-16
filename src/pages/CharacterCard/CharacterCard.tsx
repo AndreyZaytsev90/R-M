d02aab53891963b732fd14ec7df796cf6eb1ac8f
Zaycev <weddeveloper2020@gmail.com>
2026-02-16 09:03:45 +0300

    продолжаю делать компонент Select

--- src/pages/CharacterCard/CharacterCard.tsx
+++ src/pages/CharacterCard/CharacterCard.tsx
@@ -2,9 +2,20 @@ import { useNavigate } from 'react-router';
 import styles from './CharacterCard.module.css';
 import { Loading } from '@/shared';
 import { IconGoBack } from '@/assets';
+import { Select } from '@/shared/components/Select/Select';
+import { useState } from 'react';
 
 export const CharacterCard = () => {
   const navigate = useNavigate();
+  const [value, setValue] = useState('');
+
+  const optionsList = [
+    { label: 'human', value: 'Human' },
+    { label: 'alien', value: 'Alien' },
+    { label: 'humanoid', value: 'Humanoid' },
+    { label: 'animal', value: 'Animal' },
+    { label: 'robot', value: 'Robot' }
+  ];
 
   return (
     <main className={styles.container}>
@@ -15,6 +26,7 @@ export const CharacterCard = () => {
       <section className={styles.loading}>
         <Loading label='Loading character card...' size='large' />
         <Loading label='Loading character card...' size='small' />
+        <Select options={optionsList} placeholder={'Species'} value={value} onChange={setValue} size={'large'} />
       </section>
     </main>
   );
--- src/shared/components/Select/Select.tsx
+++ src/shared/components/Select/Select.tsx
@@ -14,10 +14,32 @@ type SelectProps<T> = {
   placeholder?: string;
   value: T;
   onChange: (value: T) => void;
-  DefaultOptionsComponent: ComponentType<DefaultOptionsComponentProps<T>>;
+  DefaultOptionsComponent?: ComponentType<DefaultOptionsComponentProps<T>>;
   size: 'large' | 'small';
 };
 
-export const Select = <T,>({ options, placeholder, value, DefaultOptionsComponent, size }: SelectProps) => {
-  return <select>{options}</select>;
+export const Select = <T,>({
+  options,
+  placeholder,
+  value,
+  onChange,
+  DefaultOptionsComponent,
+  size = 'large'
+}: SelectProps<T>) => {
+  const selectedOption = options.find((option) => option.value === value);
+
+  return (
+    <select className='select'>
+      <button className='select__button'>{selectedOption ? selectedOption.label : placeholder}</button>
+      <ul className='select__options'>
+        {options.map((option) => {
+          return (
+            <li key={String(option.label)} className='select__option'>
+              {option.value}
+            </li>
+          );
+        })}
+      </ul>
+    </select>
+  );
 };
