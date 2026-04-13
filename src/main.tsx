0a59693f3537787e24fa3556b081cb3f54012493
AndreyZaytsev90 <weddeveloper2020@gmail.com>
2026-04-13 21:47:50 +0300

    - добавлен ErrorBoundary

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom/client';
+import { ErrorBoundary } from 'react-error-boundary';
 import { Toaster } from 'react-hot-toast';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router';
 
@@ -6,17 +7,21 @@ import App from '@/App';
 import { CharacterCardPage, CharactersListPage } from '@/pages';
 import '@/styles/global.scss';
 
+import { ErrorFallback } from './shared/components';
+
 const root = document.getElementById('root')!;
 
 ReactDOM.createRoot(root).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<App />}>
-        <Route index element={<Navigate to='/characters' replace />} />
-        <Route path='characters' element={<CharactersListPage />} />
-        <Route path='characters/:id' element={<CharacterCardPage />} />
-      </Route>
-    </Routes>
-    <Toaster position='bottom-right' />
-  </BrowserRouter>
+  <ErrorBoundary FallbackComponent={ErrorFallback}>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}>
+          <Route index element={<Navigate to='/characters' replace />} />
+          <Route path='characters' element={<CharactersListPage />} />
+          <Route path='characters/:id' element={<CharacterCardPage />} />
+        </Route>
+      </Routes>
+      <Toaster position='bottom-right' />
+    </BrowserRouter>
+  </ErrorBoundary>
 );
